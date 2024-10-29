1a9451ef1726a3b36ef0e587a76bbb84bc66c2a3
VicZk <berezamlbb@gmail.com>
2024-10-29 16:56:09 +0200

    Fixing scrolling

--- loopstudios-landing-page-main/index.js
+++ loopstudios-landing-page-main/index.js
@@ -1,11 +1,14 @@
 let humburgerBtn = document.getElementById("humburger")
 let mobileNav = document.getElementById("mobile-nav")
 let closeBtn = document.getElementById("close")
+let body = document.body
 
 humburgerBtn.addEventListener("click", ()=>{
-    mobileNav.style.display = "block"
+    mobileNav.style.display = "block";
+    body.style.overflow = "hidden"
 })
 
 closeBtn.addEventListener("click", ()=>{
     mobileNav.style.display = "none"
+    body.style.overflow = "scroll"
 })
